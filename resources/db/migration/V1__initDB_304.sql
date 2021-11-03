e6d8b2d8f8379055d8477530094d30eb66694799
neonestea <44156406+neonestea@users.noreply.github.com>
2021-11-03 20:34:08 +0300

    Update V1__initDB_304.sql

--- resources/db/migration/V1__initDB_304.sql
+++ resources/db/migration/V1__initDB_304.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS stock
+CREATE TABLE IF NOT EXISTS stock304
 (
-    id              INT8 not null,
+    id              VARCHAR(255) not null,
     name            VARCHAR(255),
     price           decimal(10, 2),
     total_count     VARCHAR(255),
@@ -8,58 +8,60 @@ CREATE TABLE IF NOT EXISTS stock
     category        VARCHAR(255),
     primary key (id)
     );
-/*
-CREATE TABLE IF NOT EXISTS catalog_item
+
+CREATE TABLE IF NOT EXISTS catalog_item304
 (
-    id              INT8 not null,
-    product_id      INTEGER REFERENCES stock (id),
+    id              VARCHAR(255) not null,
+    product_id      VARCHAR(255) REFERENCES stock304 (id),
     amount          INTEGER(50),
     primary key (id)
     );
 
-CREATE TABLE IF NOT EXISTS shopping_cart
+CREATE TABLE IF NOT EXISTS shopping_cart304
 (
-    id       INT8 not null,
+    id       VARCHAR(255) not null,
     status   INTEGER(10),
     primary key (id)
     );
 
-CREATE TABLE IF NOT EXISTS user
+CREATE TABLE IF NOT EXISTS user304
 (
-    id              INT8 not null,
+    id              VARCHAR(255) not null,
     ip_address      INTEGER(10),
     name            VARCHAR(255),
     email           VARCHAR(255),
     phone           INTEGER(50),
-    last_basket_id  INTEGER REFERENCES shopping_cart (id),
+    last_basket_id  VARCHAR(255) REFERENCES shopping_cart304 (id),
     primary key (id)
     );
 
-CREATE TABLE IF NOT EXISTS order
+CREATE TABLE IF NOT EXISTS order304
 (
-    id          INT8 not null,
+    id          VARCHAR(255) not null,
     status      INTEGER(10),
-    basket_id   INTEGER REFERENCES shopping_cart (id),
-    user_id     INTEGER REFERENCES user (id),
+    basket_id   VARCHAR(255) REFERENCES shopping_cart304 (id),
+    user_id     VARCHAR(255) REFERENCES user304 (id),
     date        DATE,
     primary key (id)
     );
 
-CREATE TABLE IF NOT EXISTS basket_catalog_item
+CREATE TABLE IF NOT EXISTS basket_catalog_item304
 (
-    basket_id       INTEGER REFERENCES shopping_cart (id),
-    catalog_item_id INTEGER REFERENCES catalog_item (id)
+    basket_id       VARCHAR(255) REFERENCES shopping_cart304 (id),
+    catalog_item_id VARCHAR(255) REFERENCES catalog_item304 (id)
     );
 
-(CREATE TABLE IF NOT EXISTS payment
-    order_id        INTEGER REFERENCES order (id),
+CREATE TABLE IF NOT EXISTS payment304
+  (
+    order_id        VARCHAR(255) REFERENCES order304 (id),
     type            INTEGER(10),
     amount          INTEGER(10),
     time            TIME
 );
 
-(CREATE TABLE IF NOT EXISTS delivery
-    order_id        INTEGER REFERENCES order (id),
+CREATE TABLE IF NOT EXISTS delivery304
+  (
+    order_id        VARCHAR(255) REFERENCES order304 (id),
     address         VARCHAR(255),
     date            DATE
-);*/
+);
