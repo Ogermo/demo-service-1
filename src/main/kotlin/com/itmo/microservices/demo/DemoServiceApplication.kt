182a6194fa6e82c51175cf34d0e8eb2362cae19c
Ogermo <KirillZamail00@mail.ru>
2022-02-24 00:00:45 +0300

    Enable prometheus endpoint

--- src/main/kotlin/com/itmo/microservices/demo/DemoServiceApplication.kt
+++ src/main/kotlin/com/itmo/microservices/demo/DemoServiceApplication.kt
@@ -2,8 +2,10 @@ package com.itmo.microservices.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 
 @SpringBootApplication
+@EnablePrometheusEndpoint
 class DemoServiceApplication
 
 fun main(args: Array<String>) {
